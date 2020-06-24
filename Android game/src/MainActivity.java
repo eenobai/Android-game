02f58cf7a8e4cee296462bfdde0d5ed6ffeac850
eenobai <67269852+eenobai@users.noreply.github.com>
2020-06-24 11:52:24 +0300

    Add files via upload

---
+++ Android game/src/MainActivity.java
@@ -0,0 +1,27 @@
+public class MainActivity {
+
+
+// Add generateRandomNumber() here
+public int generateRandomNumber(int max){
+  double randomNumber = Math.random() * max;
+
+  int rndNumber = (int) randomNumber;
+
+  return rndNumber;
+}
+// Add getGameOverMessage() here
+
+public String getGameOverMessage(int totalCorrect, int totalQuestions){
+  String returnMsg;
+  if(totalCorrect == totalQuestions){
+    returnMsg = "You got " + totalCorrect + " right! You won!"; 
+    System.out.println(returnMsg);
+  }else{
+    returnMsg = "You got " + totalCorrect + " right out of " + totalQuestions + ". Better luck next time!";
+    System.out.println(returnMsg);
+  }
+ return returnMsg;
+}
+
+
+}
---
+++ Android game/src/Question.java
@@ -0,0 +1,38 @@
+public class Question {
+    int imageId;
+    String questionText;
+    String answer0;
+    String answer1;
+    String answer2;
+    String answer3;
+    int correctAnswer;
+    int playerAnswer;
+
+    public Question(int imageIdentifier,
+      String questionString,
+      String answerZero,
+      String answerOne,
+      String answerTwo,
+      String answerThree,
+      int correctAnswerIndex) {
+        imageId = imageIdentifier;
+        questionText = questionString;
+        answer0 = answerZero;
+        answer1 = answerOne;
+        answer2 = answerTwo;
+        answer3 = answerThree;
+        correctAnswer = correctAnswerIndex;
+        playerAnswer = -1;
+    }
+
+    // Add isCorrect() here
+
+    public boolean isCorrect(){
+        boolean isCorrectValue;
+        if(playerAnswer == correctAnswer){
+          isCorrectValue = true;
+        }else{ isCorrectValue = false;}
+        return isCorrectValue;
+    }
+
+}
